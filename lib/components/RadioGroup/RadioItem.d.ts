8cfddaeafe2022da22f3e1c32c273de60a93320d
Panayiotis Lipiridis <lipiridis@gmail.com>
2020-01-10 04:00:45 +0200

    More

--- lib/components/RadioGroup/RadioItem.d.ts
+++
@@ -1,9 +0,0 @@
-export interface OptionProps {
-    checked?: boolean;
-    onChange?: (value: any) => void;
-    label?: string | number;
-    disabled?: boolean;
-    value?: any;
-}
-declare const RadioItem: (props: OptionProps) => JSX.Element;
-export default RadioItem;
