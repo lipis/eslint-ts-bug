dab6c3411705e0f24637369cd58eedbf3bce620c
Panayiotis Lipiridis <lipiridis@gmail.com>
2020-01-10 04:03:18 +0200

    More

--- lib/components/Modal/Modal.d.ts
+++
@@ -1,8 +0,0 @@
-import { FlexProps } from "../Flex";
-interface ModalProps extends FlexProps {
-    children: JSX.Element | JSX.Element[];
-    isOpen: boolean;
-    onClose?: () => void;
-}
-declare const Modal: React.SFC<ModalProps>;
-export default Modal;
